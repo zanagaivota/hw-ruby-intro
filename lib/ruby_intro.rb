3fad4fefadeb9f405f7b4ac91bb5c96713d007f5
zanagaivota <susanagoncalves77@gmail.com>
2019-02-21 16:08:31 +0000

    fn from 1 to 5 done, 6 with errors

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -4,29 +4,78 @@
 
 def sum arr
   # YOUR CODE HERE
+  s = 0
+  if arr.length > 0
+    arr.each { |item| s += item}
+  end
+  return s
 end
 
 def max_2_sum arr
   # YOUR CODE HERE
+  s = 0
+  if arr.length == 1
+    s = arr[0]
+  elsif arr.length > 1
+    s += arr.max
+    arr.delete_at(arr.index(s))
+    s += arr.max
+  end
+  return s
 end
+
+
+# def max_2_sum arr
+  # YOUR CODE HERE
+#   s = 0
+#   test = []
+#   if arr.length == 1
+#     s = arr[0]
+#   elsif arr.length > 1
+#     test = arr.shift(2)
+#     arr.each {|item| test.delete_at(test.index(test.push(item).min)) }
+#     s = test.sum
+#   end
+#   return s
+# end
 
 def sum_to_n? arr, n
   # YOUR CODE HERE
+  l = arr.length-1
+  v = false
+  if arr.length > 1
+    for i in 0..l
+      for j in i+1..l
+        if arr[i] + arr[j] == n
+          return true
+        else
+          j += 1
+        end
+      i += 1
+      end
+    end
+  end
+  return v
 end
 
 # Part 2
 
 def hello(name)
   # YOUR CODE HERE
+  "Hello, "+name
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  /\A[a-z&&[^aeoiu]].*/i === s
 end
 
 def binary_multiple_of_4? s
   # YOUR CODE HERE
+  s.delete!(" ")
+  /^[01]+$/ === s ? s.to_i%4 == 0 : false  
 end
+
+
 
 # Part 3
 
